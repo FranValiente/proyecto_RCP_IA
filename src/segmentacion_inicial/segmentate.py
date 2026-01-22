daba288909baab4fe0a77a232089a38aa5e7eb1d
SimIA <SimIA@DESKTOP-G7UJOU2>
2026-01-22 10:34:52 +0100

    Choosing large YOLO model for pose estimation

--- src/segmentacion_inicial/segmentate.py
+++ src/segmentacion_inicial/segmentate.py
@@ -1,12 +1,14 @@
 from ultralytics import YOLO
 import torch
 
-model = YOLO("yolo26n-pose.pt")
+model = YOLO("yolo26l-pose.pt")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model.to(device)
 
 path = r'C:\Users\SimIA\Documents\proyecto_RCP_IA\data\videos\20260119-102413-v13222-5.mp4'
 
-results = model(path)
-print(type(results))
-print(results)
+results = model.predict(source=path,
+                        save=True,
+                        batch=12,
+                        classes=[0], # person
+                        max_det=7) # pacient, 5 roles, UCI
